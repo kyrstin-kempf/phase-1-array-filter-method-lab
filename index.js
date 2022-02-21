dafe2ac160a10272358308eadf75a8df5a755932
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-02-21 15:00:36 -0500

    commit tree fa014ea16cdb50281bdc1fac3cd092e5b7fb5455

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+
+function findMatching(drivers,string) {
+    return drivers.filter(el => el.toUpperCase() === string.toUpperCase())
+}
+
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(name => name.substring(0,1) === string.substring(0,1))
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(property => property.name === string)
+}
